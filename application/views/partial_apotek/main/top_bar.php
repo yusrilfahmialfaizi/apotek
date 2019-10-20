dcaeb04a309b30fc6391571b7937d5b565e8959b
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2019-10-20 22:04:18 +0700

    delete

--- application/views/partial_apotek/main/top_bar.php
+++ application/views/partial_apotek/main/top_bar.php
@@ -4,7 +4,7 @@
             <div class="navbar-header">
                 <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                 <a href="javascript:void(0);" class="bars"></a>
-                <a class="navbar-brand" href="index.html">Apotek Said Hasan Farma</a>
+                <a class="navbar-brand" href="<?php echo base_url()?>">Apotek Said Hasan Farma</a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
