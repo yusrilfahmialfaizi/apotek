a50cd8dc8c375d0c0f8ba41aa4096ef56177b66c
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2019-10-22 13:35:50 +0700

    nn

--- application/views/content_apotek/Kasir/kasir.php
+++ application/views/content_apotek/Kasir/kasir.php
@@ -22,7 +22,7 @@
                         </ul>
                     </div>
                     <!-- Default Size -->
-                    <div class="modal fade" id="defaultModal" tabindex="-1" role="dialog">
+                    <div class="modal fade" id="defaultModal" role="dialog">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
@@ -55,8 +55,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="row clearfix">
+                                        <!-- </div>
+                                        <div class="row clearfix"> -->
                                             <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
                                                 <label for="nama_generic">Nama Generic</label>
                                             </div>
@@ -91,8 +91,8 @@
                                                     </select>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="row clearfix">
+                                        <!-- </div>
+                                        <div class="row clearfix"> -->
                                             <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
                                                 <label for="per">Per</label>
                                             </div>
@@ -127,8 +127,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="row clearfix">
+                                        <!-- </div>
+                                        <div class="row clearfix"> -->
                                             <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
                                                 <label for="harga_barang">Harga</label>
                                             </div>
@@ -144,6 +144,133 @@
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-link waves-effect">Tambah</button>
+                                    <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">Tutup</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal fade" id="editModal" role="dialog">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="modal-title" id="defaultModalLabel">Edit Keranjang</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="total_harga">ID Barang</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7">
+                                                <div class="form-group">
+                                                    <select class="form-control show-tick" data-live-search="true">
+                                                        <option>Hot Dog</option>
+                                                        <option>Burger, Shake and a Smile</option>
+                                                        <option>Sugar, Spice and all things nice</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="nama_paten">Nama Paten</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="number" id="nama_paten" name="nama_paten" class="form-control" placeholder="Nama Paten" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        <!-- </div>
+                                        <div class="row clearfix"> -->
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="nama_generic">Nama Generic</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" id="nama_generic" name="nama_generic" class="form-control" placeholder="Nama Generic" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="nama_pabrik">Nama Pabrik</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" id="nama_pabrik" name="nama_pabrik" class="form-control" placeholder="Nama Pabrik" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="expired">Expired</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7">
+                                                <div class="form-group">
+                                                    <select class="form-control show-tick" id="expired" class="expired">
+                                                        <option value="">--Pilih--</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        <!-- </div>
+                                        <div class="row clearfix"> -->
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="per">Per</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7" id="per" name="per">
+                                                <div class="form-group">
+                                                    <select class="form-control show-tick">
+                                                        <option value="">--Pilih--</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="stok">Stok</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="number" id="stok" name="stok" min="0" class="form-control" placeholder="Stok " required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="qty">Qty</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="number" id="qty" name="qty" class="form-control" placeholder="Qty" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        <!-- </div>
+                                        <div class="row clearfix"> -->
+                                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                <label for="harga_barang">Harga</label>
+                                            </div>
+                                            <div class="col-lg-4 col-md-10 col-sm-8 col-xs-7">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="number" id="harga_barang" name="harga_barang" min="0" class="form-control" placeholder="Rp. " required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-link waves-effect">Edit</button>
                                     <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">Tutup</button>
                                 </div>
                             </div>
@@ -218,7 +345,7 @@
                                                     <td>
                                                         <div class="form-line">
                                                             <!-- <div class="form-line"> -->
-                                                            <button type="button" class="btn btn-sm bg-yellow waves-effect">
+                                                            <button type="button" class="btn btn-sm bg-yellow waves-effect" data-toggle="modal" data-target="#editModal">
                                                                 <i class="material-icons">edit</i>
                                                             </button>
                                                             <!-- </div> -->
