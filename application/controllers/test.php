cc80587a5200d08fad6497e235f649046834f6ad
jojobejo <maulanamalik6696@gmail.com>
2019-11-18 21:59:16 +0700

    grafik perbulan
    encik ngoding :)

---
+++ application/controllers/test.php
@@ -0,0 +1,8 @@
+<? php
+class Login Function Index(){
+	$this->load->view('halaman_login');
+}
+
+public function proses_login(){
+	
+}
---
+++ application/models/Kasir/Grafik_model.php
@@ -0,0 +1,15 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	/**
+	 * 
+	 */
+	class Grafik_model extends CI_Model
+	{
+		
+		function __construct()
+		{
+			# code...
+			parent::__construct();
+		}
+	}
+?>
