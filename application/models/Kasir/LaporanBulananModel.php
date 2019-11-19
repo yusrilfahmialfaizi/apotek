cb281108e6045240df02f7f6a5be20424c79b099
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2019-11-19 11:35:45 +0700

    laporan

--- application/models/Kasir/LaporanBulananModel.php
+++ application/models/Kasir/LaporanBulananModel.php
@@ -1,35 +1,33 @@
-<?php 
-	defined('BASEPATH') OR exit('No direct script access allowed');
-	/**
-	 * 
-	 */
-	class LaporanBulananModel extends CI_Model
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ *
+ */
+class LaporanBulananModel extends CI_Model
+{
+
+	function __construct()
 	{
-		
-		function __construct()
-		{
-			# code...
-			parent::__construct();
-		}
-
-		function ambil_data()
-		{
-			return $this->db->get("laporan_bulanan")->result();
-		}
-		function get_laporan()
-		{
-			return $this->db->get("laporan_bulanan")->result();
-		}
-		function getTransaksiMonth($year,$month)
-		{
-			$this->db->where('YEAR(tanggal)',$year);
-			$this->db->where('MONTH(tanggal)',$month);
-			return $this->db->get('laporan_bulanan')->result();
-
-		}
-		function getYear()
-		{
-			return $this->db->query("Select DISTINCT YEAR(tanggal) as year From laporan_bulanan")->result();
-		}
+		# code...
+		parent::__construct();
 	}
-?>
+
+	function ambil_data()
+	{
+		return $this->db->get("laporan_bulanan")->result();
+	}
+	function get_laporan()
+	{
+		return $this->db->get("laporan_bulanan")->result();
+	}
+	function getTransaksiMonth($year, $month)
+	{
+		$this->db->where('YEAR(tanggal)', $year);
+		$this->db->where('MONTH(tanggal)', $month);
+		return $this->db->get('laporan_bulanan')->result();
+	}
+	function getYear()
+	{
+		return $this->db->query("Select DISTINCT YEAR(tanggal) as year From laporan_bulanan")->result();
+	}
+}
