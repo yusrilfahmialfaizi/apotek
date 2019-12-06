58c1480b10816ec48d2c26aeb46d7800d5629e54
Alex Rudi Herlambang <alexrudiherlambang98@gmail.com>
2019-12-06 11:31:05 +0700

    dashboard

--- application/controllers/Apoteker/Dashboard.php
+++ application/controllers/Apoteker/Dashboard.php
@@ -1,5 +1,5 @@
 <?php
-	defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 	/**
 	 *
 	 */
@@ -10,6 +10,7 @@
 		{
 			# code...
 			parent::__construct();
+			$this->load->model("Dashboard_model");
 		}
 
 		function index()
@@ -17,10 +18,16 @@
 			if($this->session->userdata('status') != "Login" || $this->session->userdata("jabatan") != "Apoteker"){
 				redirect("login");
 			}
+			$data['jumlah_user'] = $this->Dashboard_model->jumlah_user();
+			$data['pendapatan_harian'] = $this->Dashboard_model->jumlah_pendapatan_harian();
+			$data['pendapatan_bulanan'] = $this->Dashboard_model->jumlah_pendapatan_bulanan();
+			$data['pendapatan_tahunan'] = $this->Dashboard_model->jumlah_pendapatan_tahunan();
+			$data['transaksi_harian'] = $this->Dashboard_model->jumlah_transaksi_harian();
+			$data['total_transaksi'] = $this->Dashboard_model->total_transaksi();
 			$this->load->view("partials/main/header/header_apoteker");
 			$this->load->view("content/apoteker/dashboard");
 			$this->load->view("partials/main/footer");
 		}
 
 	}
-?>
+	?>
--- application/controllers/Kasir/Dashboard.php
+++ application/controllers/Kasir/Dashboard.php
@@ -10,6 +10,7 @@
 		{
 			# code...
 			parent::__construct();
+			$this->load->model("Dashboard_model");
 		}
 
 		function index()
@@ -17,8 +18,14 @@
 			if($this->session->userdata('status') != "Login" || $this->session->userdata("jabatan") != "Kasir"){
 				redirect("login");
 			}
+			$data['jumlah_user'] = $this->Dashboard_model->jumlah_user();
+			$data['pendapatan_harian'] = $this->Dashboard_model->jumlah_pendapatan_harian();
+			$data['pendapatan_bulanan'] = $this->Dashboard_model->jumlah_pendapatan_bulanan();
+			$data['pendapatan_tahunan'] = $this->Dashboard_model->jumlah_pendapatan_tahunan();
+			$data['transaksi_harian'] = $this->Dashboard_model->jumlah_transaksi_harian();
+			$data['total_transaksi'] = $this->Dashboard_model->total_transaksi();
 			$this->load->view("partials/main/header/header_kasir");
-			$this->load->view("content/kasir/dashboard");
+			$this->load->view("content/kasir/dashboard", $data);
 			$this->load->view("partials/main/footer");
 		}
 
